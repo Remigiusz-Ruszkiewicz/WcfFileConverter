382811012a122e7e16ffed3450581ad295ad8eda
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-10-08 18:31:50 +0200

    Add project files.

---
+++ WcfFileConverter/FileConverterService.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfFileConverter
+{
+    public class FileConverterService : IFileConverterService
+    {
+        public FileModel ConvertToCSV(FileModel file)
+        {
+            var name = file.Name.Replace(".txt", ".csv");
+            var content = file.Content
+                .Replace("|", ";")
+                .Replace(",", Environment.NewLine);
+            return new FileModel
+            {
+                Name = name,
+                Content = content
+            };
+        }
+    }
+}
---
+++ WcfFileConverter/IFileConverterService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfFileConverter
+{
+
+    [ServiceContract]
+    public interface IFileConverterService
+    {
+
+        [OperationContract]
+        FileModel ConvertToCSV(FileModel file);
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class FileModel
+    {
+
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Content { get; set; }
+    }
+}
